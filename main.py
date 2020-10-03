de3e0f1d48c36eb7d165323ec69db50047668e7f
Dae Choi <dchoi@domanic.io>
2020-10-03 13:51:33 -0400

    Updates plot function

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 2, 10)
 
 fig, ax = plt.subplots()
-ax.plot([1, 2, 3, 4], [1, 4, 2, 3])
+ax.plot(x, x, label='linear')
+ax.plot(x, x ** 2, label='quadratic')
+ax.plot(x, x ** 3, label='cubic')
+
+ax.set_xlabel('x label')
+ax.set_ylabel('y label')
+
+ax.set_title('Simple Plot')
+ax.legend()
 plt.show()
+
+
+def my_plotter(ax, data1, data2, param_dict):
+    """
+    A helper function to make a graph
+
+    :param ax: Axes
+        Axes to draw to
+    :param data1: array
+        The x data
+    :param data2: array
+        The y data
+
+    :param param_dict:  dict
+        Dictionary of kwargs to pass to ax.plot
+
+    :return:
+        out: list
+            list of artists added
+    """
+    out = ax.plot(data1, data2, **param_dict)
+    return out
